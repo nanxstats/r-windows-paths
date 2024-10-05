08ead4fdde6603f591c717aea7735d0eeb8de71d
Nan Xiao <me@nanx.me>
2024-10-05 17:28:08 -0400

    Use Cascadia Code

--- images/banner.sh
+++ images/banner.sh
@@ -4,11 +4,11 @@ magick -size 2048x734 \
   -define gradient:angle=330 \gradient:#0d0551-#30e9ff \
   -gravity center \
   -pointsize 100 \
-  -font 'Jetbrains-Mono-Bold' \
+  -font 'Cascadia-Code-SemiBold' \
   -fill white \
   -annotate +0-80 'r-windows-paths' \
   -pointsize 40 \
-  -font 'Jetbrains-Mono-SemiBold' \
+  -font 'Cascadia-Code-SemiBold' \
   -annotate +0+60 'R · Rtools · TinyTeX · MiKTeX' \
   -annotate +0+130 'Pandoc · Quarto · Git' \
-  png:- | pngquant - --force --output banner.png
+  png:- | pngquant - --force --output images/banner.png
